93d9a3d4648d2068d2df641af8035cee088b6a90
aldiruli <aldiruli666@gmail.com>
2018-06-13 09:49:19 +0700

    third

--- config/daftar.php
+++ config/daftar.php
@@ -3,10 +3,11 @@
   require_once "koneksi.php";
 
   $nama_customer=$_POST['nama_customer'];
+  $email=$_POST['email'];
   $password=$_POST['password'];
   $no_tlp=$_POST['no_tlp'];
 
-    $con= mysqli_query($koneksi, "INSERT INTO customer (nama_customer, password, no_tlp)
-    values ('$nama_customer', '$password', '$no_tlp')");
+    $con= mysqli_query($koneksi, "INSERT INTO customer (nama_customer, email, password, no_tlp)
+    values ('$nama_customer', '$email', '$password', '$no_tlp')");
     echo "<script>alert('terima kasih telah mendaftar');window.location='../index.php'</script>";
 ?>
